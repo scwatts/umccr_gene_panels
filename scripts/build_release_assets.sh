a4e96592e0352f692bc62a5fd84c33c0b721cc04
Stephen Watts <hello@stephen.ac>
2024-02-22 14:02:49 +1100

    Add release asset build script

---
+++ scripts/build_release_assets.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+release_version=00.00.0
+
+
+# Set up build directory
+build_dp=build/${release_version}
+
+[[ -e ${build_dp}/ ]] && rm -r ${build_dp}/
+mkdir -p ${build_dp}/
+
+
+# Build panel data tarball
+data_dn=panel-data-v${release_version}
+data_dp=${build_dp}/${data_dn}
+
+mkdir -p ${data_dp}/
+
+rsync -aP somatic_panel/4_panel_data/output/ ${data_dp}/somatic/
+rsync -aP germline_panel/3_panel_data/output/ ${data_dp}/germline/
+rsync -aP --exclude='fusion_database.tsv' fusion_panel/1_panel_data/output/ ${data_dp}/fusion/
+
+pushd ${build_dp}/
+tar -czvf ${data_dn}.tar.gz ${data_dn}/
+popd
+
+rm -r ${data_dp}/
+
+
+# Panels
+# NOTE(SW): fusion panel is currently the somatic panel, so is skipped
+cp somatic_panel/3_final_panel/final_panel.tsv ${build_dp}/somatic_panel-v${release_version}.tsv
+cp germline_panel/2_final_panel/final_panel.tsv ${build_dp}/germline_panel-v${release_version}.tsv
